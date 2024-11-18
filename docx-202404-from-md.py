7feb0c36a252ab4cfcfbfe35a30a3722cc5f93c2
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2024-11-18 12:14:44 +0900

    Support demonstration of block type

--- docx-202404-from-md.py
+++ docx-202404-from-md.py
@@ -300,6 +300,8 @@ def process_file(md_file, output_docx, base_dir=None):
         md_content = md_content.replace("{ojs}", "")
         md_content = md_content.replace("{python}", "")
         md_content = md_content.replace("\\`", "`")
+        md_content = md_content.replace("\\{ojs\\}", "{ojs}")
+        md_content = md_content.replace("\\{python\\}", "{python}")
         with open(md_tempfile.name, "w") as f:
             f.write(md_content)
     html_tempfile = tempfile.NamedTemporaryFile(delete=False, suffix=".html")
